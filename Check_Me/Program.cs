46126efb6b9ca0cbb58dd0e71d184f0b36548a28
Asus <Asus@LAPTOP-3SQIPS1B>
2024-09-30 17:01:38 +0500

    Add project files.

---
+++ Check_Me/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//        Console.Write("M (Qatoralar soni):");
+//        int M = int.Parse(Console.ReadLine());
+//        Console.Write("N (ustunlar soni): ");
+//        int N = int.Parse(Console.ReadLine());
+//        //a) ixtiyoriy sonlar bilan matritsani toldirish 
+//        int[,] matrix = new int[M, N];
+//        Random rand = new Random();
+
+//        for (int i = 0; i < M; i++)
+//        {
+//            for (int j = 0; j < N; j++)
+//            {
+//                matrix[i, j] = rand.Next(1, 10); 
+//                Console.Write(matrix[i, j] + " ");
+//            }
+//            Console.WriteLine();
+//        }
+
+//        // b) Matritsani saralash)
+//        List<int> list = new List<int>();
+//        for (int i = 0; i < M; i++)
+//        {
+//           for (int j = 0; j < N; j++)
+//             {
+//               list.Add(matrix[i, j]);
+//            }
+//        }
+
+//        list.Sort();
+//        Console.WriteLine("\nSaralangan matritsa:");
+//        int index = 0;
+//        for (int i = 0; i < M; i++)
+//        {
+//            for (int j = 0; j < N; j++)
+//            {
+//                matrix[i, j] = list[index++];
+//                Console.Write(matrix[i, j] + " ");
+//            }
+//            Console.WriteLine();
+//        }
+
+//        // c) Matritsada toq elementlar summasini topish
+//        int sum = 0;
+//        for (int i = 0; i < M; i++)
+//        {
+//            for (int j = 0; j < N; j++)
+//            {
+//                if (matrix[i, j] % 2 != 0) // Agar son toq bo'lsa
+//                {
+//                    sum += matrix[i, j];
+//                }
+//            }
+//        }
+//        Console.WriteLine($"\nMatritsadagi toq elementlar yig'indisi: {sum}");
+
+
+
+
+
+//// d) Matritsada takrorlangan elementlarning indekslarini chiqarish
+//Dictionary<int, List<(int, int)>> elementIndices = new Dictionary<int, List<(int, int)>>();
+
+//for (int i = 0; i < M; i++)
+//{
+//    for (int j = 0; j < N; j++)
+//    {
+//        int value = matrix[i, j];
+//        if (!elementIndices.ContainsKey(value))
+//        {
+//            elementIndices[value] = new List<(int, int)>();
+//        }
+//        elementIndices[value].Add((i, j));
+//    }
+//}
+
+//Console.WriteLine("\nTakrorlangan elementlar va ularning indekslari:");
+//foreach (var y in elementIndices)
+//{
+//    if (y.Value.Count > 1)
+//    {
+//        Console.WriteLine($"Element {y.Key}:");
+//        foreach (var x in y.Value)
+//        {
+//            Console.WriteLine($"  Indeks: ({x.Item1}, {x.Item2})");
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+using System;
+using System.Linq;
+
+public class RandomMatrix
+{
+    private int[,] matrix;
+    private int M;
+    private int N;
+
+    public RandomMatrix(int M, int N)
+    {
+        this.M = M;
+        this.N = N;
+        matrix = new int[M, N];
+        GenerateMatrix();
+    }
+
+    private void GenerateMatrix()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matrix[i, j] = rand.Next(1, 10); 
+            }
+        }
+    }
+
+    public void SortMatrix()
+    {
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                for (int k = 0; k < M; k++)
+                {
+                    for (int l = 0; l < N; l++)
+                    {
+                        if (matrix[i, j] < matrix[k, l])
+                        {
+                            int temp = matrix[i, j];
+                            matrix[i, j] = matrix[k, l];
+                            matrix[k, l] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void DisplayMatrix()
+    {
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qator sonini kiriting: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Ustun sonini kiriting: ");
+        int N = int.Parse(Console.ReadLine());
+
+        RandomMatrix matrix = new RandomMatrix(M, N); 
+
+        Console.WriteLine("\nOriginal Matrix:");
+        matrix.DisplayMatrix();
+
+        matrix.SortMatrix(); 
+        Console.WriteLine("\nSaralangan Matrix:");
+        matrix.DisplayMatrix();
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Avtomobil
+{
+    public string IshlabChiqaruvchi { get; set; }
+    public string Model { get; set; }
+    public string Rangi { get; set; }
+    public decimal Narxi { get; set; }
+
+    public Avtomobil(string ishlabChiqaruvchi, string model, string rangi, decimal narxi)
+    {
+        IshlabChiqaruvchi = ishlabChiqaruvchi;
+        Model = model;
+        Rangi = rangi;
+        Narxi = narxi;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Avtomobil> avtomobillar = new List<Avtomobil>
+        {
+            new Avtomobil("Chevrolet", "Cobalt", "Oq", 10000),
+            new Avtomobil("Chevrolet", "Malibu", "Qora", 15000),
+            new Avtomobil("Toyota", "Camry", "Oq", 12000),
+            new Avtomobil("Honda", "Civic", "Qizil", 8000),
+            new Avtomobil("Ford", "Focus", "Qora", 7000),
+            new Avtomobil("BMW", "X5", "Oq", 20000),
+            new Avtomobil("Audi", "A4", "Moviy", 9000),
+            new Avtomobil("Mercedes", "C-Class", "Qora", 18000),
+            new Avtomobil("Hyundai", "Elantra", "Oq", 11000),
+            new Avtomobil("Kia", "Optima", "Yashil", 6000)
+        };
+
+        while (true)
+        {
+            Console.WriteLine("\nTanlovingizni kiriting:");
+            Console.WriteLine("1 - Barcha avtomobillar ro'yxati");
+            Console.WriteLine("2 - Mashina rangini tanlash");
+            Console.WriteLine("3 - Narxi 6000$ dan qimmmat va 12000$ dan arzon bo'lgan avtomobillar ro'yxati");
+            Console.WriteLine("0 - Chiqish");
+
+            int tanlov = int.Parse(Console.ReadLine());
+
+            switch (tanlov)
+            {
+                case 1:
+                    BarchaAvtomobillar(avtomobillar);
+                    break;
+                case 2:
+                    OqQoraAvtomobillar(avtomobillar);
+                    break;
+                case 3:
+                    NarxOraliqdagiAvtomobillar(avtomobillar, 6000, 12000);
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.WriteLine("Noto'g'ri tanlov. Iltimos, qayta urinib ko'ring.");
+                    break;
+            }
+        }
+    }
+
+    public static void BarchaAvtomobillar(List<Avtomobil> avtomobillar)
+    {
+        Console.WriteLine("Barcha avtomobillar:");
+        foreach (var avtomobil in avtomobillar)
+        {
+            Console.WriteLine($"{avtomobil.IshlabChiqaruvchi} {avtomobil.Model} - {avtomobil.Rangi} - {avtomobil.Narxi}$");
+        }
+    }
+
+    public static void OqQoraAvtomobillar(List<Avtomobil> avtomobillar)
+    {
+        var oqQoraAvtomobillar = avtomobillar.Where(a => a.Rangi == "Oq" || a.Rangi == "Qora").ToList();
+        Console.WriteLine("Oq va Qora avtomobillar:");
+        foreach (var avtomobil in oqQoraAvtomobillar)
+        {
+            Console.WriteLine($"{avtomobil.IshlabChiqaruvchi} {avtomobil.Model} - {avtomobil.Rangi} - {avtomobil.Narxi}$");
+        }
+    }
+
+    public static void NarxOraliqdagiAvtomobillar(List<Avtomobil> avtomobillar, decimal startPrice, decimal endPrice)
+    {
+        var narxOraliqdagiAvtomobillar = avtomobillar.Where(a => a.Narxi > startPrice && a.Narxi < endPrice).ToList();
+        Console.WriteLine("\nNarxi 6000$ dan qimmmat va 12000$ dan arzon bo'lgan avtomobillar:");
+        foreach (var avtomobil in narxOraliqdagiAvtomobillar)
+        {
+            Console.WriteLine($"{avtomobil.IshlabChiqaruvchi} {avtomobil.Model} - {avtomobil.Rangi} - {avtomobil.Narxi}$");
+        }
+    }
+}
